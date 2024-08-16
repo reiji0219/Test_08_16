9d89a522e6134f279f97cc8795392574b0bd98ef
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-16 20:11:42 +0900

    Test_08_16

---
+++ src/demo_05/Interface.java
@@ -0,0 +1,5 @@
+package demo_05;
+
+public interface Interface {
+	public void execute();
+}
--- src/demo_05/Test_08_16_home.java
+++ src/demo_05/Test_08_16_home.java
@@ -4,10 +4,12 @@ public class Test_08_16_home
 {
 	public static void main( String[] args )
 	{
-		
-		
-		
+		Calc16 calc = new Calc16( "Poly-morphism" );
+		calc.process( () -> {
+
+			Interface save = Calc16.create();
+				save.execute();
+			
+		});
 	}
-	
-	
 }
