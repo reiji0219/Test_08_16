fb083b7c30f67e9cc56b561517eb44c6c59131fb
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-19 21:37:48 +0900

    Test_08_19_job

--- src/demo_05/Test_08_19_job.java
+++ src/demo_05/Test_08_19_job.java
@@ -1,5 +1,20 @@
 package demo_05;
 
-public class Test_08_19_job{
+public class Test_08_19_job
+{
+	public static void main( String[] args )
+	{
+		Calc19 calc = new Calc19( "Poly-morphism" );
+		calc.process( () -> {
+
+			Interface save = Calc19.create();
+				save.execute();
+		});
+		
+		
+		
+
+			}
+	
 	
 }
