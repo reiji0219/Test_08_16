cb22584f31a370d7acecaf9a21be5c43ea216d72
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-23 23:11:20 +0900

    Test_08_23_job

--- src/demo_05/Test_08_23_job.java
+++ src/demo_05/Test_08_23_job.java
@@ -5,12 +5,12 @@ public class Test_08_23_job
 	public static void main( String[] args )
 	{
 	Calc23 calc = new Calc23( "Poly-morphism" );
-	calc.process( () -> {
-		System.out.println( "関心事を記述" );
-		
-		Interface save = Calc23.create();
-			save.execute();
-	});
+		calc.process( () -> {
+			System.out.println( "関心事を記述" );
+			
+			Interface save = Calc23.create();
+				save.execute();
+		});
 	
 	}
 }
