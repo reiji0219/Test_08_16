34828c618d1b67ee7a6473412c92a9fc4b513b62
Reiji yamaguchi <e5nqm-git@yahoo.co.jp>
2024-08-28 09:28:48 +0900

    Test_08_28_home

---
+++ src/demo_05/Test_08_28_home.java
@@ -0,0 +1,27 @@
+package demo_05;
+
+public class Test_08_28_home 
+{
+	public static void main( String[] args )
+	{
+		Calc28 calc = new Calc28( "Poly-morphism" );
+		calc.process( () -> {
+			System.out.println( "関心事を記述" );
+			
+			Interface save = Calc28.create();
+				save.execute();
+				
+		});
+		
+		
+		
+		
+		
+		
+		
+
+			
+	}
+	
+	
+}
